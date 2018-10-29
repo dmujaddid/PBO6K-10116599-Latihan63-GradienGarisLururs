d710a8370338ce0e31f2fad9b07454c7297717ae
Roby <robyeka@email.unikom.ac.id>
2018-10-29 13:07:37 +0700

    First commit

---
+++ PBO6K-10116599-Latihan61-BangunRuang/src/pbo6k/pkg10116599/latihan61/bangunruang/PBO6K10116599Latihan61BangunRuang.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan61.bangunruang;
+
+/**
+ *
+ * @author eka
+ */
+public class PBO6K10116599Latihan61BangunRuang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
