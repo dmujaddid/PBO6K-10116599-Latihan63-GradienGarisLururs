5c88a35343abae21abd3dad5ec2c359fcf73c5fb
Roby <robyeka@email.unikom.ac.id>
2018-10-31 04:49:25 +0700

    ngoding

---
+++ PBO6K-10116599-Latihan62-LivingThing/src/pbo6k/pkg10116599/latihan62/livingthing/Human.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan62.livingthing;
+
+/**
+ *
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program LivingThing.
+ */
+public class Human {
+    
+}
