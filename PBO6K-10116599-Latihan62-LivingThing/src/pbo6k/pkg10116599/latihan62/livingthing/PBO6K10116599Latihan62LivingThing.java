63836950eaf2a578e19ece04476c416de2cb2458
Roby <robyeka@email.unikom.ac.id>
2018-10-31 04:38:06 +0700

    identitas

--- PBO6K-10116599-Latihan62-LivingThing/src/pbo6k/pkg10116599/latihan62/livingthing/PBO6K10116599Latihan62LivingThing.java
+++ PBO6K-10116599-Latihan62-LivingThing/src/pbo6k/pkg10116599/latihan62/livingthing/PBO6K10116599Latihan62LivingThing.java
@@ -7,7 +7,12 @@ package pbo6k.pkg10116599.latihan62.livingthing;
 
 /**
  *
- * @author eka
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program LivingThing.
+ * 
  */
 public class PBO6K10116599Latihan62LivingThing {
 
